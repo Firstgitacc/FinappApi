633bfe21da1bcbde581280511380338671d0fb5a
Rudresh <rudra.pensiotech@gmail.com>
2024-12-29 15:56:23 +0530

    dashboard controller for dates.js

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -3,15 +3,19 @@
 const createDashboard = async(req, res)=>{
     try {
         const {name, amount, fromDate, toDate } = req.body
-
+        const formattedFromDate = new Date(fromDate);
+        const formattedToDate = new Date(toDate);
         const dashboard = new Dashboard({
             name,
             amount,
-            fromDate,
-            toDate
+            fromDate: formattedFromDate,
+            toDate: formattedToDate
         });
-        await dashboard.save();
-        res.status(201).json(dashboard)     
+        res.status(201).json({
+            ...dashboard.toObject(),
+            formattedFromDate: moment(formattedFromDate).format('YYYY-MM-DD'),
+            formattedToDate: moment(formattedToDate).format('YYYY-MM-DD')
+        }); 
     } catch (error) {
         console.log("There is an error",error);
         res.status(500).json({message:'Server error'})

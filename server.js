d7593055d6589921ac83b4ca79efde62458c33dc
Firstgitacc <45707000+Firstgitacc@users.noreply.github.com>
2024-12-29 09:20:13 +0530

    site name changed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON bodies
 app.use(cors({
    // origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-   origin: ['https://myfinanaceapp.netlify.app', 'http://localhost:3000'],
+   origin: ['https://myfinancekhm.netlify.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));

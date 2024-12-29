0a864866a2b4337c4157c7df78c96bab7928afe6
Rudresh <rudra.pensiotech@gmail.com>
2024-12-29 08:51:27 +0530

    site name changed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(cors({
-   // origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+   // origin: process.env.FRONTEND_URL || 'http://localhost:3000', site name changed
    origin: ['https://myfinancekhm.netlify.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
